0fd494c04802041db34cce6554643a9b1ef4b0b0
AyushKukshal <ayushkukshal@gmail.com>
2026-05-09 21:16:04 +0530

    Time: 8 ms (9.83%), Space: 10.2 MB (73.31%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+
+        int x = 0;
+        int y = 0;
+
+        for(char ch : moves) {
+            if(ch == 'U')
+                y++;
+            else if(ch == 'D')
+                y--;
+            else if(ch == 'L')
+                x--;
+            else if(ch == 'R')
+                x++;
+        }
+        bool ans;
+        if(x == 0 && y == 0)
+            ans = true;
+        else
+            ans = false;
+        return ans;
+    }
+};
